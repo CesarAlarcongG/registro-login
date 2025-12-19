20474f9295107444acae0e5e8a9f78c37edd7e51
CesarAlarcongG <cesarjoelalarconguzman@gmail.com>
2025-12-19 11:15:55 -0500

    feat: la contraseña se encripta cuando se registra en la BD

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,10 +1,10 @@
-import { HttpStatus, Injectable, InternalServerErrorException } from '@nestjs/common';
+import { BadRequestException, HttpStatus, Injectable, InternalServerErrorException } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { InjectModel } from '@nestjs/mongoose';
-import { UserModule } from './user.module';
 import { Model } from 'mongoose';
 import { User } from './entities/user.entity';
+import { hash } from 'bcryptjs';
 
 @Injectable()
 export class UserService {
@@ -13,11 +13,21 @@ export class UserService {
     @InjectModel(User.name) private readonly userModel: Model<User>,
   ){}
 
-  create(createUserDto: CreateUserDto) {
-    const user  = this.userModel.create(createUserDto);
+  async create(createUserDto: CreateUserDto) {
+
+    var password = createUserDto.password;
+    
+    if (!password) {
+      throw new BadRequestException('La contraseña es obligatoria');
+    }
+
+    createUserDto.password = await hash(password, 10);
+    
+    const user  = await this.userModel.create(createUserDto);
     if(user == null){
       throw new InternalServerErrorException();
     }
+  
     return HttpStatus.CREATED;
   }
 
